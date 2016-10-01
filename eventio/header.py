a18e8ae6062a9fd235afce3ceb6863f937b840c4
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-10-01 17:17:34 +0200

    Handle endianness by raising Error for BigEndian

--- eventio/header.py
+++ eventio/header.py
@@ -1,11 +1,13 @@
+import struct
 from collections import namedtuple
+import logging
 
 from .tools import read_ints
 
+log = logging.getLogger(__name__)
+
 TypeInfo = namedtuple('TypeInfo', 'type version user extended')
-
-
-SYNC_MARKER = -736130505
+SYNC_MARKER_INT_VALUE = -736130505
 
 
 def unpack_type(_type):
@@ -29,17 +31,41 @@ def extend_length(extended, length):
     return length
 
 
-def is_sync(i):
-    return i == SYNC_MARKER
+def parse_sync_bytes(sync):
+    ''' returns the endianness as given by the sync byte '''
+
+    int_value, = struct.unpack('<i', sync)
+    if int_value == SYNC_MARKER_INT_VALUE:
+        log.debug('Found Little Endian byte order')
+        return '<'
+
+    int_value, = struct.unpack('>i', sync)
+    if int_value == SYNC_MARKER_INT_VALUE:
+        log.debug('Found Big Endian byte order')
+        return '>'
+
+    raise ValueError(
+        'Sync must be 0xD41F8A37 or 0x378A1FD4. Got: {}'.format(sync)
+    )
 
 
-def read_header(f, top_level):
+def read_header(f, parent):
     _start_point = f.tell()
 
-    if top_level:
-        sync, _type, _id, length = read_ints(4, f)
+    if parent is None:
+        sync = f.read(4)
+        try:
+            endianness = parse_sync_bytes(sync)
+        except ValueError:
+            f.seek(_start_point)
+            raise
     else:
-        _type, _id, length = read_ints(3, f)
+        endianness = parent.header.endianness
+
+    if endianness == '>':
+        raise NotImplementedError('Big endian byte order is not supported by this reader')
+
+    _type, _id, length = read_ints(3, f)
 
     _type = unpack_type(_type)
     only_sub_objects, length = unpack_length(length)
@@ -48,13 +74,9 @@ def read_header(f, top_level):
         extended, = read_ints(1, f)
         length = extend_length(extended, length)
 
-    if top_level and not is_sync(sync):
-        f.seek(_start_point)
-        raise ValueError('Header sync value 0xD41F8A37 not found')
-
     _tell = f.tell()
     return_value = (
-        is_sync(sync) if top_level else True,
+        endianness,
         _type.type,
         _type.version,
         _type.user,
@@ -70,11 +92,11 @@ def read_header(f, top_level):
 
 HeaderBase = namedtuple(
     'HeaderBase',
-    'is_sync type version user extended only_sub_objects length id tell'
+    'endianness type version user extended only_sub_objects length id tell'
 )
 
 
 class ObjectHeader(HeaderBase):
-    def __new__(cls, f, top_level=True):
-        self = super().__new__(cls, *read_header(f, top_level))
+    def __new__(cls, f, parent=None):
+        self = super().__new__(cls, *read_header(f, parent))
         return self
