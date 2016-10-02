bdf754cc1ed2b2ea115c42fc756fb5e558b0736e
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-10-02 15:25:53 +0200

    Add test for CorsikaTelescopeDefition, use pytest.approx

--- eventio/tests/test_iact.py
+++ eventio/tests/test_iact.py
@@ -3,6 +3,8 @@ import pkg_resources
 from os import path
 import numpy as np
 
+from pytest import approx
+
 
 def test_file_has_correct_types():
     testfile = pkg_resources.resource_filename(
@@ -30,9 +32,24 @@ def test_read_run_header():
     f = eventio.EventIOFile(testfile)
     run_header = f[0]
 
-    assert np.isclose(run_header['energy range'][0], 5.0)
-    assert np.isclose(run_header['energy range'][1], 100.0)
-    assert np.isclose(run_header['slope of energy spectrum'], -2.7)
+    assert run_header['energy range'][0] == approx(5.0)
+    assert run_header['energy range'][1] == approx(100.0)
+    assert run_header['slope of energy spectrum'] == approx(-2.7)
+
+
+def test_telescope_definition():
+    from eventio.iact import CorsikaTelescopeDefinition
+    testfile = pkg_resources.resource_filename(
+        'eventio', path.join('resources', 'one_shower.dat')
+    )
+    f = eventio.EventIOFile(testfile)
+    telescope_definition = f[2]
+    assert isinstance(telescope_definition, CorsikaTelescopeDefinition)
+    assert telescope_definition.num_telescopes == 1
+    assert telescope_definition['x'][0] == approx(0.0)
+    assert telescope_definition['y'][0] == approx(0.0)
+    assert telescope_definition['z'][0] == approx(2500.0)
+    assert telescope_definition['r'][0] == approx(2500.0)
 
 
 def test_file_has_at_least_one_event():
