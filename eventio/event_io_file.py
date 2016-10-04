718b2c09cdbd1c43d26dff2bd1b2a74329b7a489
Dominik Neise <neised@phys.ethz.ch>
2016-10-04 18:29:58 +0200

    remove stuff not needed to pass the test
    EventIoObject was a nice trick to make the repr of the list smaller,
    since it was hiding the payload.

--- eventio/event_io_file.py
+++ eventio/event_io_file.py
@@ -2,36 +2,7 @@ import struct
 from .object_header import ObjectHeader
 from io import BytesIO
 
-class EventIoObject:
-    def __init__(self, payload):
-        self.payload = payload
-
-def read_all_objects(f, toplevel=True):
-    '''f is something like io.BufferedIOBase'''
-    objects = []
-    while True:
-        try:
-            header = ObjectHeader(f, toplevel)
-            payload = f.read(header.length)
-            if not header.only_sub_objects:
-                objects.append((header, EventIoObject(payload)))
-            else:
-                sub_objects = read_all_objects(BytesIO(payload), toplevel=False)
-                objects.append((header, sub_objects))
-
-        except ValueError:
-            warnings.warn('File seems to be truncated')
-            break
-        except struct.error:
-            break
-    return objects
-
-class EventIOFile:
-    def __init__(self, path):
-        toplevel = True
-        with open(path, 'rb') as f:
-            self.objects = read_all_objects(f, toplevel=True)
-
+__all__ = ['object_headers', 'yield_objects']
         
 def read_all_object_headers(f, toplevel=True):
     '''f is something like io.BufferedIOBase'''
@@ -66,7 +37,7 @@ def yield_all_objects(f, previous_headers=None, toplevel=True):
             header = ObjectHeader(f, toplevel)
             payload = f.read(header.length)
             if not header.only_sub_objects:
-                yield previous_headers + [header], EventIoObject(payload)
+                yield previous_headers + [header], payload
             else:
                 for o in yield_all_objects(BytesIO(payload), previous_headers + [header], toplevel=False):
                     yield o
