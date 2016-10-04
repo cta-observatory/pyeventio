bb4adec36d1b9f8b2aae11cec928324ac1e6eb04
Dominik Neise <neised@phys.ethz.ch>
2016-10-04 18:22:05 +0200

    tidy up init

--- eventio/__init__.py
+++ eventio/__init__.py
@@ -1,6 +1,4 @@
 from .base import EventIOFile
 from .iact import IACTFile
-from .object_header import ObjectHeader
-from .event_io_file import EventIOFile
 
-__all__ = ['EventIOFile', 'IACTFile', 'ObjectHeader']
+__all__ = ['EventIOFile', 'IACTFile']
