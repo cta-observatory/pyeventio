17fe1018ab0919d5d7b009d5a90c366be09b6904
Dominik Neise <neised@phys.ethz.ch>
2016-10-05 08:36:26 +0200

    revert ObjectHeader a bit, reduce magic constants:
     * before: ObjectHeader was a "verbose" namedtuple with a custom `__new__` method.
      Now I changed it partly back. ObjectHeader is a normal namedtuple again.
      Instead of inheriting from it and giving it a custom `__new__` again, which is
      prevents anybody from creating an ObjectHeader directly (testing nightmare)
      I provided a `make_ObjectHeader` function (think of it as a factory), which
      makes ObjectHeaders from file-likes.
      This is very simply code, no inheritance at all, no special function override,
      and it is still very easy to use.
      Also it is shorter, than the verbose version. aka less ode to read and maintain.
    
     * magic constants:
      In `parse_sync_bytes` constants were everywhere. Now there is one: 0xD41F8A37
      All the rest is derived from it, also in the exception message.
      Also its a bit shorter.

--- eventio/object_header.py
+++ eventio/object_header.py
@@ -10,8 +10,6 @@ from functools import namedtuple
 TypeInfo = namedtuple('TypeInfo', 'type version user extended')
 
 
-SYNC_MARKER_INT_VALUE = -736130505
-
 def unpack_type(_type):
     t = _type & 0xffff
     version = (_type & 0xfff00000) >> 20
@@ -33,119 +31,77 @@ def extend_length(extended, length):
     return length
 
 
+LITTLE_ENDIAN_MARKER = 0xD41F8A37
+BIG_ENDIAN_MARKER = struct.unpack("<I", struct.pack(">I", LITTLE_ENDIAN_MARKER))[0]
 def parse_sync_bytes(sync):
     ''' returns the endianness as given by the sync byte '''
 
-    int_value, = struct.unpack('<i', sync)
-    if int_value == SYNC_MARKER_INT_VALUE:
-        log.debug('Found Little Endian byte order')
+    int_value = struct.unpack('<I', sync)[0]
+    if int_value == LITTLE_ENDIAN_MARKER:
         return '<'
-
-    int_value, = struct.unpack('>i', sync)
-    if int_value == SYNC_MARKER_INT_VALUE:
-        log.debug('Found Big Endian byte order')
+    elif int_value == BIG_ENDIAN_MARKER:
         return '>'
+    else:
+        raise ValueError(
+            'Sync must be 0x{0:X} or 0x{1:X}. Got: {2:X}'.format(
+                LITTLE_ENDIAN_MARKER, BIG_ENDIAN_MARKER, int_value)
+        )
 
-    raise ValueError(
-        'Sync must be 0xD41F8A37 or 0x378A1FD4. Got: {}'.format(sync)
+
+def make_ObjectHeader(f, toplevel=True):
+    '''return an ObjectHeader read from a file.
+
+    Depending on `toplevel` read 3 or 4 ints from the file
+    and create an ObjectHeader from it.
+
+    Keyword Arguments:
+    f -- a file like object, supporting read and seek(also backwards)
+    toplevel -- boolean (default: True)
+    '''
+    _start_point = f.tell()
+
+    if toplevel is True:
+        sync = f.read(4)
+        try:
+            endianness = parse_sync_bytes(sync)
+        except ValueError:
+            f.seek(_start_point)
+            raise
+        level = 0
+    else:
+        endianness = None
+        level = None
+
+    _type, _id, length = read_ints(3, f)
+
+    _type = unpack_type(_type)
+    only_sub_objects, length = unpack_length(length)
+
+    if _type.extended:
+        extended, = read_ints(1, f)
+        length = extend_length(extended, length)
+
+    _tell = f.tell()
+    return ObjectHeader(
+        endianness,
+        _type.type,
+        _type.version,
+        _type.user,
+        _type.extended,
+        only_sub_objects,
+        length,
+        _id,
+        _tell,
+        level,
     )
 
-class ObjectHeader(tuple):
-    'ObjectHeader(endianness, type, version, user, extended, only_sub_objects, length, id, data_field_first_byte, level)'
 
-    __slots__ = ()
-
-    _fields = ('endianness', 'type', 'version', 'user', 'extended', 'only_sub_objects', 'length', 'id', 'data_field_first_byte', 'level')
-
-    def __new__(_cls, f, toplevel=True):
-        _start_point = f.tell()
-
-        if toplevel is True:
-            sync = f.read(4)
-            try:
-                endianness = parse_sync_bytes(sync)
-            except ValueError:
-                f.seek(_start_point)
-                raise
-            level = 0
-        else:
-            endianness = None
-            level = None
-
-        if endianness == '>':
-            raise NotImplementedError('Big endian byte order is not supported by this reader')
-
-        _type, _id, length = read_ints(3, f)
-
-        _type = unpack_type(_type)
-        only_sub_objects, length = unpack_length(length)
-
-        if _type.extended:
-            extended, = read_ints(1, f)
-            length = extend_length(extended, length)
-
-        _tell = f.tell()
-
-        return _tuple.__new__(_cls, (
-            endianness,
-            _type.type,
-            _type.version,
-            _type.user,
-            _type.extended,
-            only_sub_objects,
-            length,
-            _id,
-            _tell,
-            level,
-        ))
-
-
-
-    @classmethod
-    def _make(cls, iterable, new=tuple.__new__, len=len):
-        'Make a new ObjectHeader object from a sequence or iterable'
-        result = new(cls, iterable)
-        if len(result) != 10:
-            raise TypeError('Expected 10 arguments, got %d' % len(result))
-        return result
-
-    def _replace(_self, **kwds):
-        'Return a new ObjectHeader object replacing specified fields with new values'
-        result = _self._make(map(kwds.pop, ('endianness', 'type', 'version', 'user', 'extended', 'only_sub_objects', 'length', 'id', 'data_field_first_byte', 'level'), _self))
-        if kwds:
-            raise ValueError('Got unexpected field names: %r' % list(kwds))
-        return result
-
-    def __repr__(self):
-        'Return a nicely formatted representation string'
-        return self.__class__.__name__ + '(endianness=%r, type=%r, version=%r, user=%r, extended=%r, only_sub_objects=%r, length=%r, id=%r, data_field_first_byte=%r, level=%r)' % self
-
-    def _asdict(self):
-        'Return a new OrderedDict which maps field names to their values.'
-        return OrderedDict(zip(self._fields, self))
-
-    def __getnewargs__(self):
-        'Return self as a plain tuple.  Used by copy and pickle.'
-        return tuple(self)
-
-    endianness = _property(_itemgetter(0), doc='Alias for field number 0')
-
-    type = _property(_itemgetter(1), doc='Alias for field number 1')
-
-    version = _property(_itemgetter(2), doc='Alias for field number 2')
-
-    user = _property(_itemgetter(3), doc='Alias for field number 3')
-
-    extended = _property(_itemgetter(4), doc='Alias for field number 4')
-
-    only_sub_objects = _property(_itemgetter(5), doc='Alias for field number 5')
-
-    length = _property(_itemgetter(6), doc='Alias for field number 6')
-
-    id = _property(_itemgetter(7), doc='Alias for field number 7')
-
-    data_field_first_byte = _property(_itemgetter(8), doc='Alias for field number 8')
-
-    level = _property(_itemgetter(9), doc='Alias for field number 9')
-
+ObjectHeader = namedtuple(
+    'ObjectHeader',
+    [
+        'endianness', 'type', 'version', 'user', 'extended',
+        'only_sub_objects',  'length', 'id', 'data_field_first_byte',
+        'level'
+    ]
+)
 
