db58e497226adbc79aa92cc09046932d774a425f
Dominik Neise <neised@phys.ethz.ch>
2016-10-05 17:35:46 +0200

    objects have no seek, so we don't have to test it anymore, yeah

--- eventio/tests/test_objects.py
+++ eventio/tests/test_objects.py
@@ -6,29 +6,6 @@ testfile = pkg_resources.resource_filename(
     'eventio', path.join('resources', 'one_shower.dat')
 )
 
-
-def test_tell():
-    f = eventio.EventIOFile(testfile)
-
-    obj = f[1]
-    obj.seek(0)
-
-    assert obj.tell() == 0
-    obj.read(4)
-    assert obj.tell() == 4
-
-
-def test_seek():
-    f = eventio.EventIOFile(testfile)
-
-    obj = f[1]
-
-    obj.seek(0)
-    pos = obj.seek(0, 2)
-
-    assert pos == obj.header.length
-
-
 def test_level():
     f = eventio.EventIOFile(testfile)
 
