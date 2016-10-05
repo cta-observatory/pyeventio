84c3298d44a807d578a1ae46a0667dff5222306e
Dominik Neise <neised@phys.ethz.ch>
2016-10-05 17:19:25 +0200

    move import into test, and general import up.

--- eventio/tests/test_low_level.py
+++ eventio/tests/test_low_level.py
@@ -2,6 +2,7 @@ import pkg_resources
 import eventio
 import struct
 from os import path
+from pytest import raises
 
 one_shower = pkg_resources.resource_filename(
     'eventio', path.join('resources', 'one_shower.dat')
@@ -11,18 +12,15 @@ three_with_reuse = pkg_resources.resource_filename(
     'eventio', path.join('resources', '3_gammas_reuse_5.dat')
 )
 
-import eventio.object_header as oh
-from pytest import raises
 def test_parse_sync_bytes():
+    import eventio.object_header as oh
     assert '<' == oh.parse_sync_bytes(struct.pack('I', oh.LITTLE_ENDIAN_MARKER))
     assert '>' == oh.parse_sync_bytes(struct.pack('I', oh.BIG_ENDIAN_MARKER))
     with raises(ValueError):
         oh.parse_sync_bytes(struct.pack('I', 0))
 
-
-from eventio.event_io_file import objects
-
 def test_objects_have_headers_and_payload():
+    from eventio.event_io_file import objects
 
     for testfile_path in (one_shower, three_with_reuse):
         with open(testfile_path) as testfile:
