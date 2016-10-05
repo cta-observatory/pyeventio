23df214792620a775f2623fda59b23e6a7725c64
Dominik Neise <neised@phys.ethz.ch>
2016-10-05 17:58:32 +0200

    adjust open_file tests to new low_level_api

--- eventio/tests/test_open_file.py
+++ eventio/tests/test_open_file.py
@@ -19,36 +19,37 @@ def test_can_find_one_shower_dat():
 
 
 def test_can_open_file():
-    testfile = pkg_resources.resource_filename(
+    testfile_path = pkg_resources.resource_filename(
         'eventio', path.join('resources', 'one_shower.dat')
     )
-    eventio.EventIOFile(testfile)
+    with open(testfile_path, 'rb') as testfile:
+        eventio.objects(testfile)
 
 
-def test_file_is_iterable():
-    testfile = pkg_resources.resource_filename(
+def test_can_open_file():
+    testfile_path = pkg_resources.resource_filename(
         'eventio', path.join('resources', 'one_shower.dat')
     )
-    f = eventio.EventIOFile(testfile)
-    for event in f:
-        pass
-
+    with open(testfile_path, 'rb') as testfile:
+        f = eventio.objects(testfile)
+        for event in f:
+            pass
 
 def test_file_has_correct_types():
-    testfile = pkg_resources.resource_filename(
+    testfile_path = pkg_resources.resource_filename(
         'eventio', path.join('resources', 'one_shower.dat')
     )
-    f = eventio.EventIOFile(testfile)
-    types = [o.header.type for o in f]
-
-    assert types == [1200, 1212, 1201, 1202, 1203, 1204, 1209, 1210]
+    with open(testfile_path, 'rb') as testfile:
+        types = [ tuple(h.type for h in obj.headers) for obj in eventio.objects(testfile)]
+        assert types == [(1200,), (1212,), (1201,), (1202,), (1203,), (1204, 1205), (1209,), (1210,)]
 
 
 def test_types_gzipped():
-    testfile = pkg_resources.resource_filename(
+    import gzip
+    testfile_path = pkg_resources.resource_filename(
         'eventio', path.join('resources', 'one_shower.dat.gz')
     )
-    f = eventio.EventIOFile(testfile)
-    types = [o.header.type for o in f]
-
-    assert types == [1200, 1212, 1201, 1202, 1203, 1204, 1209, 1210]
+    with gzip.GzipFile(testfile_path, 'rb') as testfile:
+        types = [ tuple(h.type for h in obj.headers) for obj in eventio.objects(testfile)]
+        assert types == [(1200,), (1212,), (1201,), (1202,), (1203,), (1204, 1205), (1209,), (1210,)]
+        
