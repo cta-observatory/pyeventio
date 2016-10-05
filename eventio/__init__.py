1f71316a4d45f2e795c9aa43666bec4e4cfb6c30
Dominik Neise <neised@phys.ethz.ch>
2016-10-05 16:17:20 +0200

    example on how to use new low level interface

--- eventio/__init__.py
+++ eventio/__init__.py
@@ -1,4 +1,3 @@
-from .base import EventIOFile
+from .event_io_file import object_generator, object_list
 from .iact import IACTFile
-
-__all__ = ['EventIOFile', 'IACTFile']
+__all__ = ['object_generator', 'object_list', 'IACTFile']
--- eventio/iact/objects.py
+++ eventio/iact/objects.py
@@ -2,381 +2,243 @@
 import numpy as np
 from numpy.lib.recfunctions import append_fields
 import struct
+import io
 
-from ..tools import read_ints, read_from
-from ..base import EventIOObject
+from functools import namedtuple
+
 from ..exceptions import WrongSizeException
 from .parse_corsika_data import (
     parse_corsika_event_header,
     parse_corsika_run_header,
 )
 
+def parse_objects(sequence):
+    return map(parse_eventio_object, sequence)
 
-__all__ = [
-    'CorsikaRunHeader',
-    'CorsikaTelescopeDefinition',
-    'CorsikaEventHeader',
-    'CorsikaArrayOffsets',
-    'CorsikaTelescopeData',
-    'IACTPhotons',
-    'IACTLayout',
-    'IACTTriggerTime',
-    'IACTPhotoElectrons',
-    'CorsikaEventEndBlock',
-    'CorsikaRunEndBlock',
-    'CorsikaLongitudinal',
-    'CorsikaInputCard',
-]
+def parse_eventio_object(obj):
+    type_ = tuple(h.type for h in obj.headers)
+    type_to_parser = {
+        (1200,): make_CorsikaRunHeader,
+        (1201,): make_CorsikaTelescopeDefinition,
+        (1202,): make_CorsikaEventHeader,
+        (1203,): make_CorsikaArrayOffsets,
+        (1204, 1205): make_IACTPhotons,
+        (1209,): make_CorsikaEventEndBlock,
+        (1210,): make_CorsikaRunEndBlock,
+        (1211,): make_CorsikaLongitudinal,
+        (1212,): make_CorsikaInputCard,
+    }   
+    return type_to_parser.get(type_, lambda x: x)(obj)
 
 
-class CorsikaRunHeader(EventIOObject):
+def make_CorsikaRunHeader(obj):
     '''
     This object contains the corsika run header block
+    Read the data in this EventIOItem
+
+    Returns a dictionary with the items of the CORSIKA run header block
     '''
-    eventio_type = 1200
 
-    def parse_data_field(self):
-        '''
-        Read the data in this EventIOItem
+    n, = struct.unpack('i', obj.payload[:4])
+    if n != 273:
+        raise WrongSizeException('Expected 273 bytes, but found {}'.format(n))
 
-        Returns a dictionary with the items of the CORSIKA run header block
-        '''
-        self.seek(0)
-        data = self.read()
-        n, = struct.unpack('i', data[:4])
-        if n != 273:
-            raise WrongSizeException('Expected 273 bytes, but found {}'.format(n))
-
-        block = np.frombuffer(
-            data,
-            dtype=np.float32,
-            count=n,
-            offset=4,
-        )
-        return parse_corsika_run_header(block)
+    block = np.frombuffer(
+        obj.payload[4:],
+        dtype=np.float32,
+        count=n,
+    )
+    return parse_corsika_run_header(block)
 
 
-class CorsikaTelescopeDefinition(EventIOObject):
+CorsikaTelescopeDefinition = namedtuple('CorsikaTelescopeDefinition', 'n_telescopes, tel_pos')
+def make_CorsikaTelescopeDefinition(obj):
     '''
     This object contains the coordinates of the telescopes of the simulated array
+    Read the data in this EventIOItem
+
+    Returns a structured numpy array with columns (x, y, z, r)
+    with a row for each telescope
     '''
-    eventio_type = 1201
+    
+    n_telescopes = struct.unpack_from('i', obj.payload)[0]
 
-    def __init__(self, eventio_file, header, first_byte):
-        super().__init__(eventio_file, header, first_byte)
-        self.n_telescopes, = read_ints(1, self)
+    data = obj.payload[4:]
 
-    def __len__(self):
-        return self.n_telescopes
+    number_of_following_arrays = len(data) // (n_telescopes * 4)
+    if number_of_following_arrays != 4:
+        # DN: I think this cannot happen, but who knows.
+        msg = 'Number_of_following_arrays is: {}'
+        raise Exception(msg.format(number_of_following_arrays))
 
-    def parse_data_field(self):
-        '''
-        Read the data in this EventIOItem
+    dtype = np.dtype('float32')
+    block = np.frombuffer(
+        data,
+        dtype=dtype,
+        count=n_telescopes * dtype.itemsize,
+    )
+    tel_pos = np.core.records.fromarrays(
+        block.reshape(4, n_telescopes),
+        names=['x', 'y', 'z', 'r'],
+    )
+    return CorsikaTelescopeDefinition(n_telescopes, tel_pos)
 
-        Returns a structured numpy array with columns (x, y, z, r)
-        with a row for each telescope
-        '''
-        self.seek(4)
-        data = self.read()
 
-        number_of_following_arrays = len(data) // (self.n_telescopes * 4)
-        if number_of_following_arrays != 4:
-            # DN: I think this cannot happen, but who knows.
-            msg = 'Number_of_following_arrays is: {}'
-            raise Exception(msg.format(number_of_following_arrays))
+def make_CorsikaEventHeader(obj):
+    '''
+    Read the data in this EventIOItem
 
+    Returns a dictionary containing the keys of the CORSIKA event header block
+    '''
+    payload = obj.payload
+    n = struct.unpack_from('i', payload)[0]
+    if n != 273:
+        raise WrongSizeException('Expected 273 bytes, but found {}'.format(n))
+
+    block = np.frombuffer(
+        payload,
+        dtype=np.float32,
+        count=n,
+        offset=struct.calcsize('i'),
+    )
+
+    return parse_corsika_event_header(block)
+
+CorsikaArrayOffsets = namedtuple('CorsikaArrayOffsets', 'time_offset, offsets')
+def make_CorsikaArrayOffsets(obj):
+    '''
+    Read the data in this EventIOItem
+
+    Returns a structured numpy array with columns (t, x, y, weight)
+    with a row for each array. This object is used to store the
+    array position and contains one set of coordinates for each reuse.
+    '''
+
+    n_arrays, time_offset = struct.unpack_from('if', obj.payload)
+
+    n_columns = (len(obj.payload)-struct.calcsize('if')) / (n_arrays * 4)
+    assert n_columns.is_integer()
+    n_columns = int(n_columns)
+    if n_columns not in (2, 3):
+        # dneise: I think this cannot happen, but who knows.
+        msg = 'Number of offset columns should be in 2 or 3, found {}'
+        raise Exception(msg.format(n_columns))
+
+    positions = np.frombuffer(
+        obj.payload,
+        dtype=np.float32,
+        count=n_arrays * n_columns,
+        offset=struct.calcsize('if'),
+    ).reshape(n_columns, n_arrays)
+
+    if n_columns == 3:
+        weights = positions[3, :]
+    else:
+        weights = np.ones(n_arrays, dtype=np.float32)
+
+    offsets = np.core.records.fromarrays(
+        [positions[0, :], positions[1, :], weights],
+        names=['x', 'y', 'weight'],
+    )
+
+    return CorsikaArrayOffsets(time_offset, offsets)
+
+def make_IACTPhotons(obj):
+    '''
+    Read the data in this EventIOObject
+
+    Returns a numpy structured array with a record for each photon
+    and the following columns:
+        x:         x coordinate in cm
+        y:         y coordinate in cm
+        cx:        cosine of incident angle in x direction
+        cy:        cosine of incident angle in y direction
+        time:      time since first interaction in ns
+        zem:       Emission height in cm above sea level
+        lambda:    wavelength in nm
+        scattered: indicates if the photon was scattered in the atmosphere
+    '''
+
+    compact = bool(obj.headers[-1].version // 1000 == 1)
+
+    array, telescope, n_photons, n_bunches = struct.unpack_from('hhfi', obj.payload)
+
+    if compact:
+        dtype = np.dtype('int16')
+    else:
         dtype = np.dtype('float32')
-        block = np.frombuffer(
-            data,
-            dtype=dtype,
-            count=self.n_telescopes * dtype.itemsize,
-        )
-        tel_pos = np.core.records.fromarrays(
-            block.reshape(4, self.n_telescopes),
-            names=['x', 'y', 'z', 'r'],
-        )
 
-        return tel_pos
+    columns = ('x', 'y', 'cx', 'cy', 'time', 'zem', 'photons', 'lambda')
+    block = np.frombuffer(
+        obj.payload,
+        dtype=dtype,
+        count=n_bunches * len(columns),
+        offset=struct.calcsize('hhfi')
+    ).reshape(n_bunches, len(columns))
 
+    bunches = np.core.records.fromrecords(
+        block,
+        names=columns,
+    )
 
-class CorsikaEventHeader(EventIOObject):
-    ''' This Object contains the CORSIKA event header block '''
-    eventio_type = 1202
+    if compact:
+        bunches = bunches.astype([(c, 'float32') for c in columns])
+        bunches['x'] *= 0.1  # now in cm
+        bunches['y'] *= 0.1  # now in cm
 
-    def parse_data_field(self):
-        '''
-        Read the data in this EventIOItem
+        # if compact, cosines are scaled by a factor of 30000
+        bunches['cx'] /= 30000
+        bunches['cy'] /= 30000
+        #   bernloehr clips in his implementation of the reader.
+        #   I am not sure I really want that.
+        # bunches['cx'] = bunches['cx'].clip(a_min=-1., a_max=1.)
+        # bunches['cy'] = bunches['cy'].clip(a_min=-1., a_max=1.)
 
-        Returns a dictionary containing the keys of the CORSIKA event header block
-        '''
-        self.seek(0)
-        data = self.read()
-        n, = struct.unpack('i', data[:4])
-        if n != 273:
-            raise WrongSizeException('Expected 273 bytes, but found {}'.format(n))
+        bunches['time'] *= 0.1  # in nanoseconds since first interaction.
+        bunches['zem'] = np.power(10., bunches['zem']*0.001)
+        bunches['photons'] *= 0.01
 
-        block = np.frombuffer(
-            data,
-            dtype=np.float32,
-            count=n,
-            offset=4,
-        )
+    bunches = append_fields(
+        bunches,
+        data=bunches['lambda'] < 0,
+        dtypes=bool,
+        names='scattered',
+        usemask=False,
+    )
+    bunches['lambda'] = np.abs(bunches['lambda'])
 
-        return parse_corsika_event_header(block)
-
-
-class CorsikaArrayOffsets(EventIOObject):
-    eventio_type = 1203
-
-    def __init__(self, eventio_file, header, first_byte):
-        super().__init__(eventio_file, header, first_byte)
-        self.n_arrays, = read_ints(1, self)
-        self.time_offset, = read_from('f', self)
-        self.n_reuses = self.n_arrays
-
-    def parse_data_field(self):
-        '''
-        Read the data in this EventIOItem
-
-        Returns a structured numpy array with columns (t, x, y, weight)
-        with a row for each array. This object is used to store the
-        array position and contains one set of coordinates for each reuse.
-        '''
-        self.seek(8)
-        data = self.read()
-
-        n_columns = len(data) / (self.n_arrays * 4)
-        assert n_columns.is_integer()
-        n_columns = int(n_columns)
-        if n_columns not in (2, 3):
-            # dneise: I think this cannot happen, but who knows.
-            msg = 'Number of offset columns should be in 2 or 3, found {}'
-            raise Exception(msg.format(n_columns))
-
-        positions = np.frombuffer(
-            data,
-            dtype=np.float32,
-            count=self.n_arrays * n_columns,
-        ).reshape(n_columns, self.n_arrays)
-
-        if n_columns == 3:
-            weights = positions[3, :]
-        else:
-            weights = np.ones(self.n_arrays, dtype=np.float32)
-
-        return np.core.records.fromarrays(
-            [positions[0, :], positions[1, :], weights],
-            names=['x', 'y', 'weight'],
-        )
+    return bunches
 
 
-class CorsikaTelescopeData(EventIOObject):
+def make_float_block(obj):
     '''
-    A container class for the photon bunches.
-    Usually contains one photon bunch object (IACTPhotons)
-    per simulated telescope
+    No parsing yet, sorry. The meaning is defined in the CORSIKA
+    User Guide.
     '''
-    eventio_type = 1204
+    return np.frombuffer(
+        obj.payload,
+        dtype=np.dtype('float32'),
+        count=struct.unpack_from('i', obj.payload)[0],
+        offset=struct.calcsize('i')
+    )
+make_CorsikaEventEndBlock = make_float_block
+make_CorsikaRunEndBlock = make_float_block
+make_CorsikaLongitudinal = make_float_block
 
-
-class IACTPhotons(EventIOObject):
+def make_CorsikaInputCard(obj):
     '''
-    This object contains the data of the simulated cherenkov photons
-    for a single telescope
+    Read the data in this EventIOObject
+
+    Returns the CORSIKA steering card as string.
     '''
-    eventio_type = 1205
-
-    def __init__(self, eventio_file, header, first_byte):
-        super().__init__(eventio_file, header, first_byte)
-        self.compact = bool(self.header.version // 1000 == 1)
-
-        self.array, self.telescope, self.n_photons, self.n_bunches = read_from('hhfi', self)
-
-    def __repr__(self):
-        return '{}(first={}, length={}, n_bunches={})'.format(
-            self.__class__.__name__,
-            self.first_byte,
-            self.header.length,
-            self.n_bunches,
-        )
-
-    def parse_data_field(self):
-        '''
-        Read the data in this EventIOObject
-
-        Returns a numpy structured array with a record for each photon
-        and the following columns:
-            x:         x coordinate in cm
-            y:         y coordinate in cm
-            cx:        cosine of incident angle in x direction
-            cy:        cosine of incident angle in y direction
-            time:      time since first interaction in ns
-            zem:       Emission height in cm above sea level
-            lambda:    wavelength in nm
-            scattered: indicates if the photon was scattered in the atmosphere
-        '''
-        self.seek(12)
-
-        columns = ('x', 'y', 'cx', 'cy', 'time', 'zem', 'photons', 'lambda')
-
-        if self.compact:
-            dtype = np.dtype('int16')
-        else:
-            dtype = np.dtype('float32')
-
-        block = np.frombuffer(
-            self.read(self.n_bunches * len(columns) * dtype.itemsize),
-            dtype=dtype,
-            count=self.n_bunches * len(columns)
-        )
-        block = block.reshape(self.n_bunches, len(columns))
-
-        bunches = np.core.records.fromrecords(
-            block,
-            names=columns,
-        )
-
-        if self.compact:
-            bunches = bunches.astype([(c, 'float32') for c in columns])
-            bunches['x'] *= 0.1  # now in cm
-            bunches['y'] *= 0.1  # now in cm
-
-            # if compact, cosines are scaled by a factor of 30000
-            bunches['cx'] /= 30000
-            bunches['cy'] /= 30000
-            #   bernloehr clips in his implementation of the reader.
-            #   I am not sure I really want that.
-            # bunches['cx'] = bunches['cx'].clip(a_min=-1., a_max=1.)
-            # bunches['cy'] = bunches['cy'].clip(a_min=-1., a_max=1.)
-
-            bunches['time'] *= 0.1  # in nanoseconds since first interaction.
-            bunches['zem'] = np.power(10., bunches['zem']*0.001)
-            bunches['photons'] *= 0.01
-
-        bunches = append_fields(
-            bunches,
-            data=bunches['lambda'] < 0,
-            dtypes=bool,
-            names='scattered',
-            usemask=False,
-        )
-        bunches['lambda'] = np.abs(bunches['lambda'])
-
-        return bunches
-
-
-class IACTLayout(EventIOObject):
-    eventio_type = 1206
-
-
-class IACTTriggerTime(EventIOObject):
-    eventio_type = 1207
-
-
-class IACTPhotoElectrons(EventIOObject):
-    eventio_type = 1208
-
-
-class CorsikaEventEndBlock(EventIOObject):
-    eventio_type = 1209
-
-    def parse_data_field(self):
-        self.seek(0)
-        n, = read_ints(1, self)
-        if n != 273:
-            raise WrongSizeException('Expected 273 bytes, but found {}'.format(n))
-
-        dtype = np.dtype('float32')
-        block = np.frombuffer(
-            self.read(n * dtype.itemsize),
-            dtype=dtype,
-            count=n,
-        )
-
-        return block
-
-
-class CorsikaRunEndBlock(EventIOObject):
-    ''' This Object contains the CORSIKA run end block '''
-    eventio_type = 1210
-
-    def parse_data_field(self):
-        '''
-        Read the data in this EventIOObject
-
-        Returns the CORSIKA run end block as arrays of floats.
-        No parsing yet, sorry. The meaning is defined in the CORSIKA
-        User Guide.
-        '''
-
-        self.seek(0)
-        n, = read_ints(1, self)
-
-        dtype = np.dtype('float32')
-        block = np.frombuffer(
-            self.read(n * dtype.itemsize),
-            dtype=dtype,
-            count=n,
-        )
-
-        return block
-
-
-class CorsikaLongitudinal(EventIOObject):
-    ''' This Object contains the CORSIKA longitudinal shower data block '''
-    eventio_type = 1211
-
-    def __init__(self, eventio_file, header, first_byte):
-        super().__init__(eventio_file, header, first_byte)
-        self.longitudinal_data = self.parse_data_field()
-
-    def __getitem__(self, idx):
-        return self.longitudinal_data[idx]
-
-    def parse_data_field(self):
-        '''
-        Read the data in this EventIOObject
-
-        Returns the CORSIKA longitudinal shower data block as arrays of floats.
-        No parsing yet, sorry. The meaning is defined in the CORSIKA
-        User Guide.
-        '''
-        self.seek(0)
-        n, = read_ints(1, self)
-
-        dtype = np.dtype('float32')
-        block = np.frombuffer(
-            self.read(n * dtype.itemsize),
-            dtype=dtype,
-            count=n,
-        )
-
-        return block
-
-
-class CorsikaInputCard(EventIOObject):
-    ''' This Object contains the CORSIKA steering card '''
-    eventio_type = 1212
-
-    def parse_data_field(self):
-        '''
-        Read the data in this EventIOObject
-
-        Returns the CORSIKA steering card as string.
-        '''
-        self.seek(0)
-        # read number of items in the input card
-        n, = read_from('i', self)
-
-        # corsika input card is stored as null terminated strings
-        data = self.read().decode()
-        strings = data.split('\0')
-        inputcard = [
-            remove_ascii_control_characters(string)
-            for string in strings
-            if string
-        ]
-        return inputcard
+    # corsika input card is stored as null terminated strings
+    strings = obj.payload.decode().split('\0')
+    inputcard = [
+        remove_ascii_control_characters(string)
+        for string in strings
+        if string
+    ]
+    return inputcard
 
 
 def remove_ascii_control_characters(string, mapping=dict.fromkeys(range(32))):
