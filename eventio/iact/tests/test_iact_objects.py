db3f4f0a3d58fc4ae7b9eb9b39e345abff27e99c
Dominik Neise <neised@phys.ethz.ch>
2016-10-12 14:54:12 +0200

    Define more explicit object parsing API.
    comparing:
        make_CorsikaEventHeader(f[3])
    to:
        parse_object(f[3])
    
    the upper version explicitely declares that the developer
    thinks the 4th object might be an EventHeader.
    While the lower version does not explicitely make clear to the
    reader of the code, what the developer had in mind.

--- eventio/iact/tests/test_iact_objects.py
+++ eventio/iact/tests/test_iact_objects.py
@@ -3,18 +3,16 @@ import pkg_resources
 from os import path
 
 from pytest import approx
+import eventio.iact.objects as objects
 
 testfile_path = pkg_resources.resource_filename(
     'eventio', path.join('resources', 'one_shower.dat')
 )
 
 def test_corsica_event_header():
-    from eventio.iact.parse_corsika_data import CorsikaEventHeader
-    from eventio.iact.objects import parse_eventio_object
-
     with open(testfile_path, 'rb') as testfile:
         f = eventio.object_tree(testfile)
-        event_header = parse_eventio_object(f[3])
+        event_header = objects.make_CorsikaEventHeader(f[3])
 
         assert event_header.event_id == 1
         assert event_header.zenith_angle == approx(0.0)
@@ -24,10 +22,9 @@ def test_corsica_event_header():
 
 
 def test_telescope_definition():
-    from eventio.iact.objects import parse_eventio_object
     with open(testfile_path, 'rb') as testfile:
         f = eventio.object_tree(testfile)
-        telescope_definition = parse_eventio_object(f[2])
+        telescope_definition = objects.make_CorsikaTelescopeDefinition(f[2])
 
         assert telescope_definition.n_telescopes == 1
         assert telescope_definition.tel_pos['x'][0] == approx(0.0)
@@ -38,10 +35,9 @@ def test_telescope_definition():
 
 
 def test_corsica_array_offsets():
-    from eventio.iact.objects import parse_eventio_object
     with open(testfile_path, 'rb') as testfile:
         f = eventio.object_tree(testfile)
-        offsets_object = parse_eventio_object(f[4])
+        offsets_object = objects.make_CorsikaArrayOffsets(f[4])
 
         assert len(offsets_object.offsets) == 1
         assert offsets_object.offsets['x'][0] == approx(-506.9717102050781)
@@ -49,10 +45,9 @@ def test_corsica_array_offsets():
 
 
 def test_event_has_382_bunches():
-    from eventio.iact.objects import parse_eventio_object
     with open(testfile_path, 'rb') as testfile:
         f = eventio.object_tree(testfile)
-        telescope_events = parse_eventio_object(f[5])
+        telescope_events = objects.make_TelescopeEvents(f[5])
         assert len(telescope_events) == 1
 
         bunches = telescope_events[0]
@@ -60,10 +55,9 @@ def test_event_has_382_bunches():
 
 
 def test_bunches():
-    from eventio.iact.objects import parse_eventio_object
     with open(testfile_path, 'rb') as testfile:
         f = eventio.object_tree(testfile)
-        telescope_events = parse_eventio_object(f[5])
+        telescope_events = objects.make_TelescopeEvents(f[5])
         assert len(telescope_events) == 1
 
         bunches = telescope_events[0]
