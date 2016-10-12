b1a6eca41c348c3af12d76283c151c3552d5f82e
Dominik Neise <neised@phys.ethz.ch>
2016-10-12 08:45:01 +0200

    convert dicts to namedtuples

--- eventio/iact/parse_corsika_data.py
+++ eventio/iact/parse_corsika_data.py
@@ -1,153 +1,251 @@
 import numpy as np
+from functools import namedtuple
 
-
-def parse_corsika_event_header(event_header):
-    ''' parse the event header of a corsika file into a dict
-
-    **this function is called by parseByteStringToDict()** and a user normally
-    does not need to call this function directly.
-
-    The event header of a corsika file is a 272 float list,
-    where each number has a meaning by its position in the list.
-    This is documented in the Corsika Manual.
-    this is just moving the plain 272-float numbers from the original
-    header int a dict.
-    Not sure, if the key naming is well done :-/
-    '''
-    h = event_header
-    d = dict()
-    d['event number'] = int(round(h[1]))
-    d['particle id (particle code or A x 100 + Z for nuclei)'] = int(round(h[2]))
-    d['total energy in GeV'] = h[3]
-    d['starting altitude in g/cm2'] = h[4]
-    d['number of first target if fixed'] = h[5]
-    d['z coordinate (height) of first interaction in cm'] = h[6]
-    d['momentum in GeV/c in (x, y, -z) direction;'] = h[7:10]
-    d['angle in radian: (zenith, azimuth)'] = h[10:12]
-    n_random_number_sequences = int(round(h[12]))
-    if (n_random_number_sequences < 1) or (n_random_number_sequences > 10):
-        raise ValueError('Number of random number sequences n must be 0 < n < 11, but is: '+str(h[12]))
-    seed_info = h[13:13 + 3 * n_random_number_sequences].reshape(n_random_number_sequences, -1)
-    d['random number sequences: (seed, #calls, #billion calls)'] = seed_info
-    d['run number'] = int(round(h[43]))
-    d['date of begin run (yymmdd)'] = int(round(h[44]))
-    d['version of program'] = h[45]
-    n_obs_levels = int(round(h[46]))
-    if (n_obs_levels < 1) or (n_obs_levels > 10):
-        raise ValueError('number of observation levels n must be 0 < n < 11, but is: '+str(h[46]))
-    d['observation levels'] = h[47:47 + n_obs_levels]
-    d['slope of energy spektrum'] = h[57]
-    d['energy range'] = h[58:60]
-    d['kin. energy cutoff for hadrons in GeV'] = h[60]
-    d['kin. energy cutoff for muons in GeV'] = h[61]
-    d['kin. energy cutoff for electrons in GeV'] = h[62]
-    d['energy cutoff for photons in GeV'] = h[63]
-    d['NFLAIN'] = h[64]
-    d['NFLDIF'] = h[65]
-    d['NFLPI0'] = h[66]
-    d['NFLPIF'] = h[67]
-    d['NFLCHE'] = h[68]
-    d['NFRAGM'] = h[69]
-    d['Earth\'s magnetic field in uT: (x,z)'] = h[70:72]
-    d['flag for activating EGS4'] = h[72]
-    d['flag for activating NKG'] = h[73]
-    d['low-energy hadr. model flag (1.=GHEISHA, 2.=UrQMD, 3.=FLUKA)'] = h[74]
-    d['high-energy hadr. model flag (0.=HDPM,1.=VENUS, 2.=SIBYLL,3.=QGSJET, 4.=DPMJET, 5.=NE X US, 6.=EPOS)'] = h[75]
-    d['CERENKOV Flag (is a bitmap --> usersguide)'] = hex(int(round(h[76])))
-    d['NEUTRINO flag'] = h[77]
-    d['CURVED flag (0=standard, 2=CURVED)'] = h[78]
-    d['computer flag (3=UNIX, 4=Macintosh)'] = h[79]
-    d['theta interval (in degree): (lower, upper edge) '] = h[80:82]
-    d['phi interval (in degree): (lower, upper edge) '] = h[82:84]
-    d['Cherenkov bunch size in the case of Cherenkov calculations'] = h[84]
-    d['number of Cherenkov detectors in (x, y) direction'] = h[85:87]
-    d['grid spacing of Cherenkov detectors in cm (x, y) direction'] = h[87:89]
-    d['length of each Cherenkov detector in cm in (x, y) direction'] = h[89:91]
-    d['Cherenkov output directed to particle output file (= 0.) or Cherenkov output file (= 1.)'] = h[91]
-    d['angle (in rad) between array x-direction and magnetic north'] = h[92]
-    d['flag for additional muon information on particle output file'] = h[93]
-    d['step length factor for multiple scattering step length in EGS4'] = h[94]
-    d['Cherenkov bandwidth in nm: (lower, upper) end'] = h[95:97]
-    n_reuse=h[97]
-    d['number i of uses of each Cherenkov event'] = n_reuse
-    core_x = h[98:98+n_reuse]
-    core_y = h[118:118+n_reuse]
-    d['core location for scattered events in cm: (x,y)'] = np.vstack((core_x,core_y)).transpose()
-    d['SIBYLL interaction flag (0.= no SIBYLL, 1.=vers.1.6; 2.=vers.2.1)'] = h[138]
-    d['SIBYLL cross-section flag (0.= no SIBYLL, 1.=vers.1.6; 2.=vers.2.1)'] = h[139]
-    d['QGSJET interact. flag (0.=no QGSJET, 1.=QGSJETOLD,2.=QGSJET01c, 3.=QGSJET-II)'] = h[140]
-    d['QGSJET X-sect. flag (0.=no QGSJET, 1.=QGSJETOLD,2.=QGSJET01c, 3.=QGSJET-II)'] = h[141]
-    d['DPMJET interaction flag (0.=no DPMJET, 1.=DPMJET)'] = h[142]
-    d['DPMJET cross-section flag (0.=no DPMJET, 1.=DPMJET)'] = h[143]
-    d['VENUS/NE X US/EPOS cross-section flag (0=neither, 1.=VENUSSIG,2./3.=NEXUSSIG, 4.=EPOSSIG)'] = h[144]
-    d['muon multiple scattering flag (1.=Moliere, 0.=Gauss)'] = h[145]
-    d['NKG radial distribution range in cm'] = h[146]
-    d['EFRCTHN energy fraction of thinning level hadronic'] = h[147]
-    d['EFRCTHN x THINRAT energy fraction of thinning level em-particles'] = h[148]
-    d['actual weight limit WMAX for thinning hadronic'] = h[149]
-    d['actual weight limit WMAX x WEITRAT for thinning em-particles'] = h[150]
-    d['max. radius (in cm) for radial thinning'] = h[151]
-    d['viewing cone VIEWCONE (in deg): (inner, outer) angle'] = h[152:154]
-    d['transition energy high-energy/low-energy model (in GeV)'] = h[154]
-    d['skimming incidence flag (0.=standard, 1.=skimming)'] = h[155]
-    d['altitude (cm) of horizontal shower axis (skimming incidence)'] = h[156]
-    d['starting height (cm)'] = h[157]
-    d['flag indicating that explicite charm generation is switched on'] = h[158]
-    d['flag for hadron origin of electromagnetic subshower on particle tape'] = h[159]
-    d['flag for observation level curvature (CURVOUT) (0.=flat, 1.=curved)'] = h[167]
-    return d
-
-
+CorsikaRunHeader = namedtuple('CorsikaRunHeader', [
+    'run_id',
+    'date_of_begin_run',
+    'corsika_version',
+    'observation_levels',
+    'energy_slope',
+    'energy_range',
+    'EGS4_flag',
+    'NKG_flag',
+    'hadron_cutoff_energy',
+    'muon_cutoff_energy',
+    'electron_cutoff_energy',
+    'photon_cutoff_energy',
+    'phyiscal_constants',
+    'inclinded_observation_plane_x',
+    'inclinded_observation_plane_y',
+    'inclinded_observation_plane_z',
+    'inclinded_observation_plane_theta',
+    'inclinded_observation_plane_phi',
+    'NSHOW',
+    'CKA',
+    'CETA',
+    'CSTRBA',
+    'scatter_range_x',
+    'scatter_range_y',
+    'HLAY',
+    'AATM',
+    'BATM',
+    'CATM',
+    'NFLAIN',
+    'NFLDIF',
+    'NFLPI0_plus_100_times_NFLPIF',
+    'NFLCHE_plus_100_times_NFRAGM',])
 def parse_corsika_run_header(run_header):
-    '''
-    parse the run header of a corsika file into a dict
-
-    **this function is called by parseByteStringToDict()** and a user normally
-    does not need to call this function directly.
-
-    The run header of a corsika file is a 272 float list,
-    where each number has a meaning by its position in the list.
-    This is documented in the Corsika Manual.
-    this is just moving the plain 272-float numbers from the original
-    header int a dict.
-    Not sure, if the key naming is well done :-/
-    '''
     h = run_header
-    d = dict()
-    d['run number'] = h[1]
-    d['date of begin run'] = int(round(h[2]))
-    d['version of program'] = h[3]
     n_obs_levels = int(round(h[4]))
     if (n_obs_levels < 1) or (n_obs_levels > 10):
         raise ValueError('number of observation levels n must be 0 < n < 11, but is: '+str(h[4]))
-    d['observation levels']=h[5:5+n_obs_levels]
-    d['slope of energy spectrum']=h[15]
-    d['energy range']=h[16:18]
-    d['flag for EGS4 treatment of em. component'] = h[18]
-    d['flag for NKG treatment of em. component'] = h[19]
-    d['kin. energy cutoff for hadrons in GeV'] = h[20]
-    d['kin. energy cutoff for muons in GeV'] = h[21]
-    d['kin. energy cutoff for electrons in GeV'] = h[22]
-    d['energy cutoff for photons in GeV'] = h[23]
-    d['phyiscal constants'] = h[24:74]
-    d['X-displacement of inclined observation plane'] = h[74]
-    d['Y-displacement of inclined observation plane'] = h[75]
-    d['Z-displacement of inclined observation plane'] = h[76]
-    d['theta angle of normal vector of inclined observation plane'] = h[77]
-    d['phi angle of normal vector of inclined observation plane'] = h[78]
-    # now some constants, I don't understand
-    d['NSHOW'] = h[93]
-    d['CKA'] = h[94:134]
-    d['CETA'] = h[134:139]
-    d['CSTRBA'] = h[139:150]
-    d['scatter range in x direction for Cherenkov'] = h[247]
-    d['scatter range in y direction for Cherenkov'] = h[248]
-    d['HLAY'] = h[249:254]
-    d['AATM'] = h[254:259]
-    d['BATM'] = h[259:264]
-    d['CATM'] = h[264:269]
-    d['NFLAIN'] = h[269]
-    d['NFLDIF'] = h[270]
-    d['NFLPI0 + 100 x NFLPIF'] = h[271]
-    d['NFLCHE + 100 x NFRAGM'] = h[272]
-    return d
+
+    return CorsikaRunHeader(
+        run_id = h[1],
+        date_of_begin_run = int(round(h[2])),
+        corsika_version = h[3],
+        observation_levels = h[5:5+n_obs_levels],
+        energy_slope = h[15],
+        energy_range = h[16:18],
+        EGS4_flag = h[18],
+        NKG_flag = h[19],
+        hadron_cutoff_energy = h[20],
+        muon_cutoff_energy = h[21],
+        electron_cutoff_energy = h[22],
+        photon_cutoff_energy = h[23],
+        phyiscal_constants = h[24:74],
+        inclinded_observation_plane_x = h[74],
+        inclinded_observation_plane_y = h[75],
+        inclinded_observation_plane_z = h[76],
+        inclinded_observation_plane_theta = h[77],
+        inclinded_observation_plane_phi = h[78],
+        NSHOW = h[93],
+        CKA = h[94:134],
+        CETA = h[134:139],
+        CSTRBA = h[139:150],
+        scatter_range_x = h[247],
+        scatter_range_y = h[248],
+        HLAY = h[249:254],
+        AATM = h[254:259],
+        BATM = h[259:264],
+        CATM = h[264:269],
+        NFLAIN = h[269],
+        NFLDIF = h[270],
+        NFLPI0_plus_100_times_NFLPIF = h[271],
+        NFLCHE_plus_100_times_NFRAGM = h[272],
+    )
+
+
+CorsikaEventHeader = namedtuple('CorsikaEventHeader', [
+    'event_id',
+    'particle_id',
+    'total_energy',
+    'starting_altitude',
+    'number_of_first_target_if_fixed',
+    'first_interaction_height',
+    'momentum_x',
+    'momentum_y',
+    'momentum_z',
+    'zenith_angle',
+    'azimuth_angle',
+    'seed_info',
+    'run_id',
+    'date_of_begin_run',
+    'corsika_version',
+    'observation_levels',
+    'energy_slope',
+    'energy_range',
+    'hadron_cutoff_energy',
+    'muon_cutoff_energy',
+    'electron_cutoff_energy',
+    'photon_cutoff_energy',
+    'NFLAIN',
+    'NFLDIF',
+    'NFLPI0',
+    'NFLPIF',
+    'NFLCHE',
+    'NFRAGM',
+    'magnetic_field_x_component',
+    'magnetic_field_z_component',
+    'EGS4_flag',
+    'NKG_flag',
+    'low_energy_hadr_model_flag',
+    'high_energy_hadr_model_flag',
+    'cherenkov_bitmap',
+    'neutrino_flag',
+    'curved_flag',
+    'unix_or_mac_flag',
+    'lower_theta_limit',
+    'upper_theta_limit',
+    'lower_phi_limit',
+    'upper_phi_limit',
+    'cherenkov_bunch_size',
+    'number_of_cherenkov_detectors_in_x_direction',
+    'number_of_cherenkov_detectors_in_y_direction',
+    'spacing_of_cherenkov_detectors_in_x_direction',
+    'spacing_of_cherenkov_detectors_in_y_direction',
+    'length_of_each_cherenkov_detector_in_x_direction',
+    'length_of_each_cherenkov_detector_in_y_direction',
+    'cherenkov_output_to_dedicated_cherenkov_output_file',
+    'angle_between_x_direction_and_magnetic_north',
+    'additional_muon_output_flag',
+    'EGS4_multiple_scattering_step_length_factor',
+    'cherenkov_bandwidth_lower_end',
+    'cherenkov_bandwidth_upper_end',
+    'n_reuse',
+    'core_location',
+    'sibyll',
+    'qgsjet',
+    'dpmjet',
+    'venus_cross_section_flag',
+    'muon_multiple_scattering_flag',
+    'nkg_radial_distribution_range',
+    'efrcthn',
+    'weight_limit_for_thinning_hadronic',
+    'weight_limit_for_thinning_em_particles',
+    'max_radius_for_radial_thinning',
+    'viewcone_inner_angle',
+    'viewcone_outer_angle',
+    'transition_energy_high_low_energy_model',
+    'skimming_incidence_flag',
+    'altitude_of_horizontal_shower_axis',
+    'starting_height',
+    'charm_generation_flag',
+    'hadron_origin_of_em_subshower_flag',
+    'observation_level_curvature_flag',
+])
+
+def parse_corsika_event_header(event_header):
+    h = event_header
+
+    n_random_number_sequences = int(round(h[12]))
+    if (n_random_number_sequences < 1) or (n_random_number_sequences > 10):
+        raise ValueError('Number of random number sequences n must be 0 < n < 11, but is: '+str(h[12]))
+
+    n_obs_levels = int(round(h[46]))
+    if (n_obs_levels < 1) or (n_obs_levels > 10):
+        raise ValueError('number of observation levels n must be 0 < n < 11, but is: '+str(h[46]))
+
+    n_reuse=int(h[97])
+    core_x = h[98:98+n_reuse],
+    core_y = h[118:118+n_reuse],
+
+    return CorsikaEventHeader(
+        event_id = int(round(h[1])),
+        particle_id = int(round(h[2])),
+        total_energy = h[3],
+        starting_altitude = h[4],
+        number_of_first_target_if_fixed = h[5],
+        first_interaction_height = h[6],
+        momentum_x = h[7],
+        momentum_y = h[8],
+        momentum_z = h[9],
+        zenith_angle = h[10],
+        azimuth_angle = h[11],
+        seed_info = h[13:13 + 3 * n_random_number_sequences].reshape(n_random_number_sequences, -1),
+        run_id = int(round(h[43])),
+        date_of_begin_run = int(round(h[44])), # make it a datetime.date!
+        corsika_version = h[45],
+        observation_levels = h[47:47 + n_obs_levels],
+        energy_slope = h[57],
+        energy_range = h[58:60],
+        hadron_cutoff_energy = h[60],
+        muon_cutoff_energy = h[61],
+        electron_cutoff_energy = h[62],
+        photon_cutoff_energy = h[63],
+        NFLAIN = h[64],
+        NFLDIF = h[65],
+        NFLPI0 = h[66],
+        NFLPIF = h[67],
+        NFLCHE = h[68],
+        NFRAGM = h[69],
+        magnetic_field_x_component = h[70],
+        magnetic_field_z_component = h[71],
+        EGS4_flag = h[72],
+        NKG_flag = h[73],
+        low_energy_hadr_model_flag = h[74],
+        high_energy_hadr_model_flag = h[75],
+        cherenkov_bitmap = hex(int(round(h[76]))),
+        neutrino_flag = h[77],
+        curved_flag = h[78],
+        unix_or_mac_flag = h[79],
+        lower_theta_limit = h[80],
+        upper_theta_limit = h[81],
+        lower_phi_limit = h[82],
+        upper_phi_limit = h[83],
+        cherenkov_bunch_size = h[84],
+        number_of_cherenkov_detectors_in_x_direction = h[85],
+        number_of_cherenkov_detectors_in_y_direction = h[86],
+        spacing_of_cherenkov_detectors_in_x_direction = h[87],
+        spacing_of_cherenkov_detectors_in_y_direction = h[88],
+        length_of_each_cherenkov_detector_in_x_direction = h[89],
+        length_of_each_cherenkov_detector_in_y_direction = h[90],
+        cherenkov_output_to_dedicated_cherenkov_output_file = h[91],
+        angle_between_x_direction_and_magnetic_north = h[92],
+        additional_muon_output_flag = h[93],
+        EGS4_multiple_scattering_step_length_factor = h[94],
+        cherenkov_bandwidth_lower_end = h[95],
+        cherenkov_bandwidth_upper_end = h[96],
+        n_reuse = n_reuse,
+        core_location = np.vstack((core_x,core_y)).transpose(),
+        sibyll = {'interaction_flag': h[138], 'cross_section_flag': h[139]},
+        qgsjet = {'interaction_flag': h[140], 'cross_section_flag': h[141]},
+        dpmjet = {'interaction_flag': h[142], 'cross_section_flag': h[143]},
+        venus_cross_section_flag = h[144],
+        muon_multiple_scattering_flag = h[145],
+        nkg_radial_distribution_range = h[146],
+        efrcthn = {'energy fraction of thinning level hadronic': h[147],
+            'energy fraction of thinning level em-particles': h[148]},
+        weight_limit_for_thinning_hadronic = h[149],
+        weight_limit_for_thinning_em_particles = h[150],
+        max_radius_for_radial_thinning = h[151],
+        viewcone_inner_angle = h[152],
+        viewcone_outer_angle = h[152],
+        transition_energy_high_low_energy_model = h[154],
+        skimming_incidence_flag = h[155],
+        altitude_of_horizontal_shower_axis = h[156],
+        starting_height = h[157],
+        charm_generation_flag = h[158],
+        hadron_origin_of_em_subshower_flag = h[159],
+        observation_level_curvature_flag = h[167],
+    )
