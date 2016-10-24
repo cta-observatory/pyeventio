45cf32b504efe97d54dffc9c0a14a1219a7ba09a
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-10-24 17:21:37 +0200

    Adjust tests to new corsika headers

--- eventio/iact/tests/test_iact_file.py
+++ eventio/iact/tests/test_iact_file.py
@@ -30,9 +30,9 @@ def test_read_run_header():
     f = eventio.IACTFile(testfile)
 
     assert hasattr(f, 'header')
-    assert f.header['energy range'][0] == approx(5.0)
-    assert f.header['energy range'][1] == approx(100.0)
-    assert f.header['slope of energy spectrum'] == approx(-2.7)
+    assert f.header.energy_range[0] == approx(5.0)
+    assert f.header.energy_range[1] == approx(100.0)
+    assert f.header.energy_slope == approx(-2.7)
 
 
 def test_run_end_block():
@@ -101,10 +101,10 @@ def test_event_header():
     event = f[0]
 
     assert hasattr(event, 'header')
-    assert event.header['event number'] == 1
-    assert event.header['angle in radian: (zenith, azimuth)'][0] == approx(0.0)
-    assert event.header['angle in radian: (zenith, azimuth)'][1] == approx(0.0)
-    assert event.header['total energy in GeV'] == approx(9.3249321)
+    assert event.header.event_id == 1
+    assert event.header.zenith_angle == approx(0.0)
+    assert event.header.azimuth_angle == approx(0.0)
+    assert event.header.total_energy == approx(9.3249321)
 
 
 def test_event_end_block():
