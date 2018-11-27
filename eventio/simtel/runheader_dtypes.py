db30561d0e4c5c97a53e1f869f4f4cbc6921d70c
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-11-27 22:47:47 +0100

    Fix runheader and add test

--- eventio/simtel/runheader_dtypes.py
+++ eventio/simtel/runheader_dtypes.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 runheader_dtype_part1 = np.dtype([
-    #(fieldname, type, shape)
+    #  (fieldname, type, shape)
     ('run', 'i4'),
     ('time', 'i4'),
     ('run_type', 'i4'),
@@ -11,21 +11,15 @@ runheader_dtype_part1 = np.dtype([
     ('offset_fov', 'f4', (2,)),
     ('conv_depth', 'f4'),
     ('conv_ref_pos', 'f4', (2,)),
-    ('ntel', 'i4'),
+    ('n_telescopes', 'i4'),
 ])
 
-def runheader_dtype_part2(ntel):
+
+def runheader_dtype_part2(n_telescopes):
     return np.dtype([
-        #(fieldname, type, shape)
-        ('tel_id', 'i2', (ntel,)),
-        ('tel_pos', 'f4', (ntel, 3)),
+        #  (fieldname, type, shape)
+        ('tel_id', 'i2', (n_telescopes,)),
+        ('tel_pos', 'f4', (n_telescopes, 3)),
         ('min_tel_trig', 'i4'),
         ('duration', 'i4', ),
-        ('spectral_index', 'f4'),
-        ('B_total', 'f4'),
-        ('B_inclination', 'f4'),
-        ('B_declination', 'f4'),
-        ('injection_height', 'f4'),
-        ('atmosphere', 'i4'),
     ])
-
