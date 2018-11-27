cb11ae067cd073263f64bf9d71f2784460c36ef6
Dominik Neise <neised@phys.ethz.ch>
2018-11-27 17:39:00 +0100

    add test to make sure we do not seek too far

--- eventio/tests/test_tools.py
+++ eventio/tests/test_tools.py
@@ -17,14 +17,17 @@ def test_read_string():
 def test_get_count():
     from eventio.tools import get_count
     testcases = [
-        #(input, output)
-        (bytes([0b0111_1111, 1, 2, 3, 4, 5, 6, 7, 8]), 0b0111_1111),
-        (bytes([0b0000_1111, 1, 2, 3, 4, 5, 6, 7, 8]), 0b0000_1111),
+        #(input, output, tell_after_done)
+        (bytes([0b0111_1111, 1, 2, 3, 4, 5, 6, 7, 8]), 0b0111_1111, 1),
+        (bytes([0b0000_1111, 1, 2, 3, 4, 5, 6, 7, 8]), 0b0000_1111, 1),
 
-        (bytes([0b1011_1111, 0b1100_1100, 2, 3, 4, 5, 6, 7, 8]), 0b0011_1111_1100_1100),
+        (bytes([0b1011_1111, 0b1100_1100, 2, 3, 4, 5, 6, 7, 8]), 0b0011_1111_1100_1100, 2),
 
-        (bytes([0b1101_1111, 0b1100_1100, 0b1010_1010, 3, 4, 5, 6, 7, 8]), 0b0001_1111_1100_1100_1010_1010),
+        (bytes([0b1101_1111, 0b1100_1100, 0b1010_1010, 3, 4, 5, 6, 7, 8]), 0b0001_1111_1100_1100_1010_1010, 3),
     ]
-    for data, expected_result in testcases:
-        assert get_count(BytesIO(data)) == expected_result
+    for data, expected_result, tell_after_done in testcases:
+        stream = BytesIO(data)
+        assert get_count(stream) == expected_result
+        # make sure we do not seek too far
+        assert stream.tell() == tell_after_done
 
