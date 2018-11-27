ee25a53b3d9719cb26df75d581d50b779d448727
Dominik Neise <neised@phys.ethz.ch>
2018-11-27 17:14:00 +0100

    first version of get_count and get_scout

--- eventio/tests/test_tools.py
+++ eventio/tests/test_tools.py
@@ -13,3 +13,18 @@ def test_read_string():
     buffer.seek(0)
 
     assert read_eventio_string(buffer) == s
+
+def test_get_count():
+    from eventio.tools import get_count
+    testcases = [
+        #(input, output)
+        (bytes([0b0111_1111, 1, 2, 3, 4, 5, 6, 7, 8]), 0b0111_1111),
+        (bytes([0b0000_1111, 1, 2, 3, 4, 5, 6, 7, 8]), 0b0000_1111),
+
+        (bytes([0b1011_1111, 0b1100_1100, 2, 3, 4, 5, 6, 7, 8]), 0b0011_1111_1100_1100),
+
+        (bytes([0b1101_1111, 0b1100_1100, 0b1010_1010, 3, 4, 5, 6, 7, 8]), 0b0001_1111_1100_1100_1010_1010),
+    ]
+    for data, expected_result in testcases:
+        assert get_count(data) == expected_result
+
--- eventio/tools.py
+++ eventio/tools.py
@@ -1,5 +1,5 @@
 import struct
-
+import numpy as np
 
 def read_eventio_string(f):
     '''Read a string from eventio file or object f
@@ -32,3 +32,36 @@ def read_from_without_position_change(fmt, f):
     result = read_from(fmt, f)
     f.seek(position)
     return result
+
+
+def get_scount(data):
+    # this is mostly a verbatim copy from eventio.c lines 1082ff
+    u = get_count(data)
+    # u values of 0,1,2,3,4,... here correspond to signed values of
+    #   0,-1,1,-2,2,... We have to test the least significant bit:
+    if (u & 1) == 1:  # Negative number;
+        return -(u >> 1) - 1
+    else:
+        return u >> 1
+
+
+def get_count(data):
+    '''this returns a python integer'''
+    a = np.frombuffer(data, dtype='B', count=9).copy()
+    b = np.zeros(8, dtype='B')
+
+    # FIXME avoid this loop to make it faster.
+    # find the most significant zero in a[0]
+    for pos_of_msb_zero in range(8)[::-1]:  # pos_of_msb_zero goes from 7..0
+        if ~a[0] & 1 << pos_of_msb_zero:
+            break
+
+    # mask away some leading ones in a[0]
+    a[0] &= (1 << (pos_of_msb_zero + 1)) - 1
+
+    # copy the interesting part from a into b and return a view
+    b[pos_of_msb_zero:] = a[0:8 - pos_of_msb_zero]  # b has a length from 1 to 8
+
+    # FIXME: I read all 9 bytes .. but I should only read as many as I need for the
+    # int .. so I should seek back.
+    return int(b.view('>u8')[0])
