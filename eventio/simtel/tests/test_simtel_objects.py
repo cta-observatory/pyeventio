6915b4fe63b0d7a55ffea3862b209eb46f069da4
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-11-28 07:52:29 +0100

    Implement reading of trackevent

--- eventio/simtel/tests/test_simtel_objects.py
+++ eventio/simtel/tests/test_simtel_objects.py
@@ -1,4 +1,7 @@
+from pkg_resources import resource_filename
 
+
+test_file = resource_filename('eventio', 'resources/gamma_test.simtel.gz')
 
 
 
@@ -17,3 +20,24 @@ def test_telid():
     assert SimTelTelEvent.type_to_telid(3305) == 205
     assert SimTelTelEvent.type_to_telid(3205) == 105
     assert SimTelTelEvent.type_to_telid(2203) == 3
+
+
+def test_track():
+    from eventio import EventIOFile
+    from eventio.simtel.objects import SimTelEvent, SimTelTrackEvent
+    with EventIOFile(test_file) as f:
+
+        # search for first event
+        o = next(f)
+        while o.eventio_type != SimTelEvent.eventio_type:
+            o = next(f)
+
+        for s in o:
+            if isinstance(s, SimTelTrackEvent):
+                break
+        else:
+            assert False, 'No Track event found'
+
+        pointing = s.parse_data_field()
+        assert 'azimuth_raw' in pointing.dtype.names
+        assert 'altitude_raw' in pointing.dtype.names
