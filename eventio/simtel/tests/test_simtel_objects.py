a8aa7c4e5d69798fde2b24b9aa454a457c1d8b19
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-11-28 09:51:00 +0100

    Merge test files

--- eventio/simtel/tests/test_simtel_objects.py
+++ eventio/simtel/tests/test_simtel_objects.py
@@ -4,6 +4,20 @@ from pkg_resources import resource_filename
 test_file = resource_filename('eventio', 'resources/gamma_test.simtel.gz')
 
 
+def test_run_heder():
+    from eventio import EventIOFile
+    from eventio.simtel import SimTelRunHeader
+
+    with EventIOFile(test_file) as f:
+        o = next(f)
+        while not isinstance(o, SimTelRunHeader):
+            o = next(f)
+
+        data = o.parse_data_field()
+        data['observer'] = b'bernlohr@lfc371.mpi-hd.mpg.de'
+        data['target'] = b'Monte Carlo beach'
+
+
 def test_2002():
     from eventio import EventIOFile
     from eventio.simtel.objects import SimTelCamSettings
