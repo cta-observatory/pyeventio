d0825c50da978dcd4aac45326f442f863ad77edf
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-11-29 13:10:38 +0100

    Add function to find all subelements

--- eventio/simtel/tests/test_simtel_objects.py
+++ eventio/simtel/tests/test_simtel_objects.py
@@ -28,6 +28,25 @@ def collect_toplevel_of_type(f, eventio_type):
     return classes_under_test
 
 
+def find_all_subcontainers(f, structure, level=0):
+    '''
+    Find all subcontainers expected in structure.
+    So if you want all AdcSums, use
+    structure = [SimTelEvent, SimTelTelEvent, SimTelTelADCSum]
+    '''
+    objects = []
+    elem = structure[level]
+
+    for o in f:
+        if isinstance(o, structure[-1]):
+            objects.append(o)
+        elif isinstance(o, elem):
+            objects.extend(find_all_subcontainers(f, structure, level + 1))
+    return objects
+
+
+
+
 def test_70():
     from eventio import EventIOFile
     from eventio.simtel.objects import History
