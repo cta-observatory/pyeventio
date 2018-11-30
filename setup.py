8a6dc9352df3674edd52a93f3b9bd73aef7549da
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-11-30 18:21:59 +0100

    Update setup, make tests pass with 3.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='eventio',
-    version='0.5.0',
+    version='0.5.1',
     description='Python read-only implementation of the EventIO file format',
     long_description=long_description,
     url='https://github.com/fact-project/pyeventio',
@@ -16,6 +16,22 @@ setup(
     package_data={'eventio': ['resources/*']},
     install_requires=[
         'numpy'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS 9',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Scientific/Engineering :: Physics',
     ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest>=3.0.0'],
