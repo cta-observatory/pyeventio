cee5b3399a5c3c1be3cb0343e300473a9dc9d9e8
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-11-30 13:49:19 +0100

    Plot all telescopes, add option to select single telescope

--- examples/plot_light_on_ground.py
+++ examples/plot_light_on_ground.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 from pkg_resources import resource_filename
 from argparse import ArgumentParser
+import numpy as np
 
 from eventio import IACTFile
 
 parser = ArgumentParser()
 parser.add_argument('-i', '--inputfile', dest='inputfile')
 parser.add_argument('-e', '--event', dest='event', type=int, default=0)
+parser.add_argument('-t', '--telescope', dest='telescope', type=int)
 
 
 def main():
@@ -17,19 +19,23 @@ def main():
     with IACTFile(args.inputfile) as f:
 
         for event in f:
-            photons = event.photon_bunches[0]
+            if args.telescope:
+                photons = [event.photon_bunches[args.telescope]]
+            else:
+                photons = list(event.photon_bunches.values())
 
             fig, ax = plt.subplots()
             ax.set_aspect(1)
             ax.set_facecolor('k')
 
-            ax.scatter(
-                x=photons['x'],
-                y=photons['y'],
-                c='w',
-                s=10,
-                lw=0,
-            )
+            for tel_photons in photons:
+                ax.scatter(
+                    x=tel_photons['x'],
+                    y=tel_photons['y'],
+                    c='w',
+                    s=10,
+                    lw=0,
+                )
 
             plt.show()
 
