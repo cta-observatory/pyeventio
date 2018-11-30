3191f29113f1a2aae82d634c9d383ece6f030bc4
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-11-30 17:56:16 +0100

    Respect telescope position in example

--- examples/plot_production_3d.py
+++ examples/plot_production_3d.py
@@ -21,17 +21,21 @@ with eventio.IACTFile(args.inputfile or testfile) as f:
         event = next(it)
 
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    ax = fig.add_axes([0, 0, 1, 1], projection='3d')
 
     obslevel = event.header.observation_levels[0]
 
-    for b in event.photon_bunches.values():
+    for pos, b in zip(f.telescope_positions, event.photon_bunches.values()):
         cz = 1 - (b['cx']**2 + b['cy']**2)
         x = b['x'] + ((b['zem'] - obslevel) / cz) * b['cx']
         y = b['y'] + ((b['zem'] - obslevel) / cz) * b['cy']
 
-        ax.scatter(x / 100, y / 100, b['zem'] / 1e5, s=3, lw=0, c='k')
+        x -= pos[0]
+        y -= pos[1]
 
+        ax.scatter(x / 100, y / 100, b['zem'] / 1e5, s=3, lw=0, c='k', alpha=0.2)
+
+    ax.view_init(10, 45)
     ax.set_xlabel('x / m')
     ax.set_ylabel('y / m')
     ax.set_zlabel('z / km')
