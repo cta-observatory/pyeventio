7a5ad6bfc4aafdfbf8f081417dc17d7e10d6f020
Dominik Neise <neised@phys.ethz.ch>
2018-12-03 20:29:02 +0100

    allow print_structure also other files

--- examples/print_structure.py
+++ examples/print_structure.py
@@ -1,6 +1,11 @@
+import sys
 from eventio import EventIOFile
 from eventio.search_utils import yield_all_objects_depth_first
 
-with EventIOFile('eventio/resources/gamma_20deg_0deg_run102___cta-prod4-sst-1m_desert-2150m-Paranal-sst-1m.simtel.gz') as f:
+try:
+    url = sys.argv[1]
+except IndexError:
+    url = 'eventio/resources/gamma_20deg_0deg_run102___cta-prod4-sst-1m_desert-2150m-Paranal-sst-1m.simtel.gz'
+with EventIOFile(url) as f:
     for o, level in yield_all_objects_depth_first(f):
         print('    ' * level, o)
