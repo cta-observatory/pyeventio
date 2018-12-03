b9949713e907d2e5a9336d8f4440acc79ac908db
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-12-03 22:44:52 +0100

    Loop through all histograms

--- examples/plot_hists.py
+++ examples/plot_hists.py
@@ -1,6 +1,6 @@
 from eventio import EventIOFile, Histograms
 import matplotlib.pyplot as plt
-from eventio.search_utils import find_type
+from eventio.search_utils import collect_toplevel_of_type
 import numpy as np
 from argparse import ArgumentParser
 
@@ -16,23 +16,23 @@ args = parser.parse_args()
 inputfile = args.inputfile[0] or default
 
 with EventIOFile(inputfile) as f:
-    o = find_type(f, Histograms)
-    hists = o.parse_data_field()
+    for o in  collect_toplevel_of_type(f, Histograms):
+        hists = o.parse_data_field()
 
-    for hist in hists:
-        plt.figure()
-        x_bins = np.linspace(hist['lower_x'], hist['upper_x'], hist['n_bins_x'] + 1)
+        for hist in hists:
+            plt.figure()
+            x_bins = np.linspace(hist['lower_x'], hist['upper_x'], hist['n_bins_x'] + 1)
 
-        if hist['n_bins_y'] > 0:
-            y_bins = np.linspace(hist['lower_y'], hist['upper_y'], hist['n_bins_y'] + 1)
+            if hist['n_bins_y'] > 0:
+                y_bins = np.linspace(hist['lower_y'], hist['upper_y'], hist['n_bins_y'] + 1)
 
-            plt.pcolormesh(x_bins, y_bins, hists[0]['data'].T)
+                plt.pcolormesh(x_bins, y_bins, hists[0]['data'].T)
 
-        else:
-            centers = 0.5 * (x_bins[:-1] + x_bins[1:])
-            plt.hist(centers, bins=x_bins, weights=hist['data'])
+            else:
+                centers = 0.5 * (x_bins[:-1] + x_bins[1:])
+                plt.hist(centers, bins=x_bins, weights=hist['data'])
 
-        plt.title(hist['title'])
-        plt.tight_layout()
+            plt.title(hist['title'])
+            plt.tight_layout()
 
 plt.show()
