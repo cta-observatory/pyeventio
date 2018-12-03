753b65a9f8dbe7ebd11d7ba0385a63997136bfdc
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-12-03 22:30:25 +0100

    Test titles

--- eventio/tests/test_histograms.py
+++ eventio/tests/test_histograms.py
@@ -7,6 +7,19 @@ prod4b_sst1m_file = resource_filename(
     'resources/gamma_20deg_0deg_run102___cta-prod4-sst-1m_desert-2150m-Paranal-sst-1m.simtel.gz'
 )
 
+titles = [
+    'Events, without weights (Ra, log10(E))',
+    'Array triggered, without weights (Ra, log10(E))',
+    'Angle to observing position (all showers), n.w.',
+    'Angle to observing position (triggered showers), n.w.',
+    'Events, without weights (Ra3d, log10(E))',
+    'Array triggered, without weights (Ra3d, log10(E))',
+    'Photons per telescope sphere, n.w.',
+    'Photons per telescope sphere (tel. triggered), n.w.',
+    'Photo-electrons per telescope, n.w.',
+    'Photo-electrons per telescope (tel. triggered), n.w.',
+]
+
 
 def test_histograms():
     from eventio import Histograms, EventIOFile
@@ -15,6 +28,11 @@ def test_histograms():
         objects = collect_toplevel_of_type(f, Histograms)
 
         for obj in objects:
-            obj.parse_data_field()
+            hists = obj.parse_data_field()
             unread = obj.read()
             assert len(unread) == 0 or all(b == 0 for b in unread)
+
+            for hist, title in zip(hists, titles):
+                assert hist['title'] == title
+
+
