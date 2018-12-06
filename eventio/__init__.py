249eb759a3efa66f6d215d1ca31a53afcbe85a13
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-12-06 16:55:14 +0100

    Import iact

--- eventio/__init__.py
+++ eventio/__init__.py
@@ -1,10 +1,17 @@
 from .base import EventIOFile, KNOWN_OBJECTS
+from . import iact
 from .iact import IACTFile
-from .simtel import SimTelFile
 from . import simtel
+from .simtel import SimTelFile
 from .histograms import Histograms
 
-__all__ = ['EventIOFile', 'IACTFile', 'SimTelFile', 'simtel']
+__all__ = [
+    'EventIOFile',
+    'IACTFile',
+    'SimTelFile',
+    'simtel',
+    'iact',
+]
 
 
 KNOWN_OBJECTS[Histograms.eventio_type] = Histograms
