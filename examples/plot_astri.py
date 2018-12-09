e5c30917a136444558624d80c0971591f2c8ebbf
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-12-09 14:07:30 +0100

    Use simtelfile for plot_astri

--- examples/plot_astri.py
+++ examples/plot_astri.py
@@ -8,50 +8,40 @@ from ctapipe.visualization import CameraDisplay
 
 from pkg_resources import resource_filename
 
-from eventio import EventIOFile
-from eventio.simtel import (
-    CameraSettings,
-    Event,
-    TelescopeEvent,
-    ADCSums
-)
+from eventio.simtel import SimTelFile
+from functools import lru_cache
 
 input_file = resource_filename(
     'eventio',
     'resources/gamma_20deg_0deg_run103___cta-prod4-sst-astri_desert-2150m-Paranal-sst-astri.simtel.gz'
 )
 
-with EventIOFile(input_file) as f:
+
+@lru_cache()
+def build_cam_geom(f, telescope_id):
+    cam_data = f.telescope_descriptions[telescope_id]['camera_settings']
+    return CameraGeometry(
+        cam_id='CAM-{}'.format(telescope_id),
+        pix_id=np.arange(cam_data['n_pixels']),
+        pix_x=cam_data['pixel_x'] * u.m,
+        pix_y=cam_data['pixel_y'] * u.m,
+        pix_area=cam_data['pixel_area'] * u.m**2,
+        pix_type='square',
+        cam_rotation=cam_data['cam_rot'] * u.rad,
+    )
+
+
+with SimTelFile(input_file) as f:
     cameras = {}
-    for o in f:
-        if isinstance(o, CameraSettings):
-            cam_data = o.parse()
 
-            if cam_data['pixel_shape'][0] == -1:
-                pixel_shape = 'hexagonal' if cam_data['n_pixels'] < 2000 else 'square'
-            else:
-                pixel_shape = 'square' if cam_data['pixel_shape'][0] else 'hexagonal'
+    for array_event in f:
+        print('Event:', array_event['event_id'])
+        for telescope_id, event in array_event['telescope_events'].items():
+            print('Telescope:', telescope_id)
 
-            cameras[o.telescope_id] = CameraGeometry(
-                cam_id='CAM-{}'.format(o.telescope_id),
-                pix_id=np.arange(cam_data['n_pixels']),
-                pix_x=cam_data['pixel_x'] *  u.m,
-                pix_y=cam_data['pixel_y'] *  u.m,
-                pix_area=cam_data['pixel_area'] * u.m**2,
-                pix_type=pixel_shape,
-                cam_rotation=cam_data['cam_rot'] * u.rad,
-            )
+            cam = build_cam_geom(f, telescope_id)
 
-        if isinstance(o, Event):
-            assert len(cameras) > 0
-            for subo in o:
-                if isinstance(subo, TelescopeEvent):
-                    for subsubo in subo:
-                        if isinstance(subsubo, ADCSums):
-                            data = subsubo.parse()
-
-                            plt.figure()
-                            cam = cameras[subo.telescope_id]
-                            disp = CameraDisplay(cam)
-                            disp.image = data['adc_sums'][0]
-                            plt.show()
+            plt.figure()
+            disp = CameraDisplay(cam)
+            disp.image = event['adc_sums'][0]
+            plt.show()
