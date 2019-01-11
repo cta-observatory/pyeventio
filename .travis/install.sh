a102697187052621e54e8b44bc02fcbe2b316f4e
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-01-11 11:11:08 +0100

    Use miniconda on osx

---
+++ .travis/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+	curl -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+	bash miniconda.sh -p $HOME/miniconda -b
+	. $HOME/miniconda/etc/profile.d/conda.sh
+	conda activate
+	conda install python=$PYTHON
+fi
