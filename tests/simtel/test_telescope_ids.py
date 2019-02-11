b409c78fc2d8c2ba43cc52f78a09ef11821848b8
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-02-11 09:58:19 +0100

    Test also on gamma test

--- tests/simtel/test_telescope_ids.py
+++ tests/simtel/test_telescope_ids.py
@@ -2,18 +2,24 @@ from eventio import EventIOFile
 from eventio.search_utils import yield_toplevel_of_type
 
 
-test_file = 'tests/resources/gamma_20deg_0deg_run103___cta-prod4-sst-astri_desert-2150m-Paranal-sst-astri.simtel.gz'
+test_files = [
+    'tests/resources/gamma_20deg_0deg_run103___cta-prod4-sst-astri_desert-2150m-Paranal-sst-astri.simtel.gz',
+    'tests/resources/gamma_test.simtel.gz',
+]
 
 
 def test_telescope_ids():
     from eventio.simtel import ArrayEvent, TelescopeEvent
 
-    with EventIOFile(test_file) as f:
+    def assert_event_has_equal_telids(event):
+        for telescope in yield_toplevel_of_type(event, TelescopeEvent):
+            for o in telescope:
+                assert o.telescope_id == telescope.telescope_id
 
-        for array_event in yield_toplevel_of_type(f, ArrayEvent):
-            for telescope_event in yield_toplevel_of_type(array_event, TelescopeEvent):
-                telescope_id = None
-                for o in telescope_event:
-                    telescope_id = telescope_id or o.telescope_id
-
-                    assert telescope_id == o.telescope_id
+    for test_file in test_files:
+        try:
+            with EventIOFile(test_file) as f:
+                for event in yield_toplevel_of_type(f, ArrayEvent):
+                    assert_event_has_equal_telids(event)
+        except EOFError:
+            pass
