2fd2cc4e5983c259296b66cef96491ecfe23bc33
Dominik Neise <neised@phys.ethz.ch>
2019-02-12 16:10:50 +0100

    remove unused and commented out function

--- eventio/simtel/objects.py
+++ eventio/simtel/objects.py
@@ -764,17 +764,6 @@ class TelescopeEventHeader(TelescopeObject):
         return event_head
 
 
-# def number_of_bits_in(n):
-#     n = (n & 0x5555555555555555) + ((n & 0xAAAAAAAAAAAAAAAA) >> 1)
-#     n = (n & 0x3333333333333333) + ((n & 0xCCCCCCCCCCCCCCCC) >> 2)
-#     n = (n & 0x0F0F0F0F0F0F0F0F) + ((n & 0xF0F0F0F0F0F0F0F0) >> 4)
-#     n = (n & 0x00FF00FF00FF00FF) + ((n & 0xFF00FF00FF00FF00) >> 8)
-#     n = (n & 0x0000FFFF0000FFFF) + ((n & 0xFFFF0000FFFF0000) >> 16)
-#     # The last & isn't strictly necessary.
-#     n = (n & 0x00000000FFFFFFFF) + ((n & 0xFFFFFFFF00000000) >> 32)
-#     return n
-
-
 def number_of_bits_in(n):
     m = 0
     for i in range(16):
