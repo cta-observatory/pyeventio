dfd78322ded40ca82ffe98b7a5c4b0afb983dbf8
Dominik Neise <neised@phys.ethz.ch>
2019-02-13 16:51:40 +0100

    test also adc_samples

--- tests/simtel/test_zero_suppression.py
+++ tests/simtel/test_zero_suppression.py
@@ -11,6 +11,14 @@ TEST_FILE_PATH = path.join(
     )
 )
 
+TEST_FILE_PATH_NORMAL = path.join(
+    environ['TEST_FILE_DIR'],
+    (
+        'gamma_20deg_180deg_run1187___cta-prod3-demo_desert-2150m-Paranal'
+        '-demo2sect_cone10.simtel.gz'
+    )
+)
+
 
 def test_adc_sums():
     pyhessio = pytest.importorskip("pyhessio")
@@ -25,3 +33,24 @@ def test_adc_sums():
                 HE_adc_sum = hessio_file.get_adc_sum(tel_id)
                 EE_adc_sum = EE['telescope_events'][tel_id]['adc_sums']
                 assert np.array_equal(HE_adc_sum, EE_adc_sum)
+
+
+def test_adc_samples():
+
+    for z, e in zip(
+        SimTelFile(TEST_FILE_PATH),
+        SimTelFile(TEST_FILE_PATH_NORMAL)
+    ):
+        for tel_id in e['telescope_events']:
+            try:
+                zas = z['telescope_events'][tel_id]['adc_samples']
+                eas = e['telescope_events'][tel_id]['adc_samples']
+                survivors = (zas != 0).all(axis=-1)
+                assert survivors.sum() > 0
+                assert np.array_equal(zas[survivors], eas[survivors])
+            except KeyError as exc:
+                print(
+                    'event:', e['event_id'],
+                    'tel:', tel_id,
+                    'KeyError:', exc
+                )
